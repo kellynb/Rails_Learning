96b9be44556ded6b7f8a8f82a5fb367e1806bf0c
kellynb <bkellyn013@gmail.com>
2019-11-03 12:01:26 -0600

    initializing a repo

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,9 @@
+class ApplicationController < ActionController::Base
+    def hello
+        render html: "hola, mundo!"
+    end
+
+    def goodbye
+        render html: "Goodbye y'all!"
+    end
+end
